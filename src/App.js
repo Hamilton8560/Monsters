a44732f294c568065820c77a35fa01e046c67ffa
David Hamilton <davidhamilton473@gmail.com>
2023-02-14 12:27:32 -0500

    Upload Monsters

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component'
+import SearchBox from './components/search-box/search-box.component'
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters:[],
+      searchField:''
+     }
+    }
+    
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then((users) => this.setState(() =>{
+          return {monsters: users}
+        },
+        () => {
+          console.log(this.state)
+        }
+        ))
+    }
+
+    onSearchChange = (event)=>{
+      const searchField=event.target.value.toLocaleLowerCase();
+    this.setState(
+      () =>{
+      return {searchField}
+    })
+  }
+
+
+  render(){
+    const{monsters,searchField}=this.state;
+    const{onSearchChange}=this;
+    
+    const filteredMonsters=monsters.filter((monster)=>{ 
+      return monster.name.toLocaleLowerCase().includes(searchField)
+  })
+  return (
+    <div className="App">
+    <h1 className="app-title">Monsters Rolodex</h1>
+    <h3 className ="author">by David Hamilton</h3>
+  
+    {/*filteredMonsters.map((monster) => {
+        return (
+          <div key ={monster.id}>
+        <h1>{monster.name}</h1>
+        </div>
+        )
+      })*/
+    }
+      <SearchBox 
+      className = 'monsters-search-box'
+      onChangeHandler = {onSearchChange} 
+      placeholder='search monsters'/>
+      <CardList monsters={filteredMonsters}/>
+    </div>
+  );
+}
+}
+export default App;
